0479cef34c236bcd541546cf08e14d4e4a689cde
Job Nestor Bahner <job.nestor@gmail.com>
2015-02-01 16:18:26 +0100

    Changed naming of object

--- pym.py
+++ pym.py
@@ -1,8 +1,22 @@
-#Bsic
+#Imports Tkinter (now available for use)
 from Tkinter import *
 
-root = Tk()
-root.title("pym")
-root.geometry("500x500")
-root.mainloop()
-
+#Sets up the basic parameters of the GUI
+tk = Tk()
+tk.title("PYM")
+tk.geometry("500x500")
+#Create the welcome screen
+app = Frame(tk)
+label = Label(app, text="Welcome! What would you like to do?")
+label.grid()
+app.grid()
+New = Button(app, text="Create new server.proprties")
+Old = Button(app, text="Import server.properties")
+New.grid()
+Old.grid()
+#Now that I have the basics, I need to add functions to these buttons
+def new2():
+     
+    
+#Mainloop keeps GUI running (ALWAYS AT BOTTOM OF SCRIPT!!)
+tk.mainloop()
